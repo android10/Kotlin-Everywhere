a0296c9657d35068cd758ab9892c8750835e1d41
Fernando Cejas <fcejas@gmail.com>
2021-11-05 10:34:21 +0100

    feat: add skeleton for movie list and movie details.

---
+++ mobile/ios-app/ios-app/Features/Movies/MovieListView.swift
@@ -0,0 +1,9 @@
+//
+//  MovieListView.swift
+//  ios-app
+//
+//  Created by Fernando Cejas on 05.11.21.
+//  Copyright © 2021 Fernando Cejas. All rights reserved.
+//
+
+import Foundation
---
+++ mobile/ios-app/ios-app/Features/Profile/ProfileView.swift
@@ -0,0 +1,26 @@
+//
+//  ProfileView.swift
+//  ios-app
+//
+//  Created by Fernando Cejas on 05.11.21.
+//  Copyright © 2021 Fernando Cejas. All rights reserved.
+//
+
+import Foundation
+
+import SwiftUI
+import shared
+
+struct ProfileView: View {
+    
+    @ObservedObject private var profileModel = ProfileModel(getProfile: GetProfile())
+    
+    var body: some View {
+        return Form {
+            Section { TextField("Name", text: $profileModel.name) }
+            Section { TextField("Email", text: $profileModel.email) }
+
+            .onAppear(perform: { profileModel.fetch() })
+        }
+    }
+}
